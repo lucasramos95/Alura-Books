cf9f84e5e8fb9f1bf6dda60ded80ae69e935098b
Lucas <lucas_ramos95@outlook.com>
2022-11-28 23:53:41 -0300

    commit #03 - Conversão promise da API

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/');
+var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+.then(resposta => resposta.json())
+.then(r => (r));
